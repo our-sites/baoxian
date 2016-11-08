84f47830ab90dd62a840b1e9ee3d55f9ee073d38
朱鹏程 <309579250@qq.com>
2016-11-08 17:24:56 +0800

    产品列表页

---
+++ ucd/www/product/1.0.0/dist/src/index.js
@@ -0,0 +1,8 @@
+define('www/product/1.0.0:index', ['www/product/1.0.0:common'], function(require, exports, module) {
+
+  require('www/product/1.0.0:common');
+  
+  // @require 'www/product/1.0.0:index.css';
+  
+
+});
---
+++ ucd/www/product/1.0.0/dist/src/product.js
@@ -0,0 +1,5 @@
+define('www/product/1.0.0:product', [], function(require, exports, module) {
+
+  
+
+});
