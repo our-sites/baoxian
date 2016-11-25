d44911348411e64c3197bd9b415b4fc4328be33d
朱鹏程 <309579250@qq.com>
2016-11-25 11:04:16 +0800

    问吧修改

---
+++ ucd/www/ask/1.0.0/dist/src/detail.js
@@ -0,0 +1,8 @@
+define('www/ask/1.0.0:detail', ['www/ask/1.0.0:common'], function(require, exports, module) {
+
+  require('www/ask/1.0.0:common');
+  
+  // @require 'www/ask/1.0.0:detail.css'
+  
+
+});
