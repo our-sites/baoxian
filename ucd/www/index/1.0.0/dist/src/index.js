3d45237dc53ad47e3c578c5a1c93db5115a01e08
朱鹏程 <309579250@qq.com>
2016-12-08 13:59:48 +0800

    首页

--- ucd/www/index/1.0.0/dist/src/index.js
+++ ucd/www/index/1.0.0/dist/src/index.js
@@ -1,5 +1,7 @@
-define('www/index/1.0.0:index', [], function(require, exports, module) {
+define('www/index/1.0.0:index', ['www/common/1.0.0:common'], function(require, exports, module) {
 
+  require('www/common/1.0.0:common');
   
+  // @require 'www/index/1.0.0:index.css'
 
 });
