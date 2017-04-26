a435aecbe1636fdb11f3bb907f023907b1ef2ccb
Cuixote <309579250@qq.com>
2017-04-26 17:10:01 +0800

    咨询弹窗

---
+++ ucd/com/chat/1.0.0/dist/src/consultation.js
@@ -0,0 +1,8 @@
+define('com/chat/1.0.0:consultation', ['com/global/1.0.0:dollar'], function(require, exports, module) {
+
+  var $ = require('com/global/1.0.0:dollar');
+  
+  
+  // @require 'com/chat/1.0.0:consultation.css'
+
+});
