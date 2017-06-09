8934ca545c9e21bc22596bcaec9bccffd393dc5e
zys <zhangyunsheng@gongchang.com>
2017-06-10 01:54:00 +0800

    tijiao

--- ucd/www/site/1.0.0/dist/src/site.js
+++ ucd/www/site/1.0.0/dist/src/site.js
@@ -2,7 +2,40 @@ define('www/site/1.0.0:site', ['www/common/1.0.0:common', 'com/global/1.0.0:doll
 
   require('www/common/1.0.0:common');
   
-  var $ = require('com/global/1.0.0:dollar');
+  var $ = require('com/global/1.0.0:dollar'),
+  	$doc = $(document);
+  
+  //表单的提交
+  $doc.on('submit', '.reserve-modal-form, .question-advisory-form', function(e) {
+  	e.preventDefault();
+  	var $this = $(this),
+  		_d = $this.serialize(),
+  		_u = $this.attr('action'),
+  		_m = $this.attr('method').toLowerCase() || 'get';
+  	
+  	$[_m](_u, _d, function(r) {
+  		if(r.status) {
+  			alert(r.msg);
+  		} else {
+  			alert(r.msg);
+  		}
+  	}, 'json');
+  });
+  
+  //滚动
+  var $list = $('.has-reserve-list');
+  var tipScroll = setInterval(function() {
+  		$list.animate({marginTop: '-44px'}, 1000,function() {
+  		
+  			var $first = $list.find('li').eq(0).clone();
+  			$list.append($first);
+  			$list.find('li:first').remove();
+  			$list.css('marginTop', 0);
+  		});
+  		
+  	}, 2000);
+  
+  
   
   
   
