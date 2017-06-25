62a8e436b9f6e56af5f7190ae2738aa17ba64ad5
zys <zhangyunsheng@gongchang.com>
2017-06-26 02:41:49 +0800

    bx页面

---
+++ ucd/www/info/1.0.0/dist/src/info.js
@@ -0,0 +1,7 @@
+define('www/info/1.0.0:info', ['www/info/1.0.0:common'], function(require, exports, module) {
+
+  require('www/info/1.0.0:common');
+  
+  // @require 'www/info/1.0.0:info.css';
+
+});
